16fcebb6c8665ce9835fb59b8d9147d359b12fbe
Mark Ulrich <mark.ulrich.86@gmail.com>
2019-05-06 04:52:46 -0500

    Updates

---
+++ precache-manifest.52891d3b3ad2e294aac772f6bdb84ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cbbd1c844bcbafe69528c026a5a47f",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d417bfaa692654ef3df7",
+    "url": "/fcc-pomodoro-clock/static/css/main.afc86138.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/fcc-pomodoro-clock/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d417bfaa692654ef3df7",
+    "url": "/fcc-pomodoro-clock/static/js/main.5b6595a8.chunk.js"
+  },
+  {
+    "revision": "ec7b801c7c9eecf06fdf",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.a052a505.js"
+  }
+]);
