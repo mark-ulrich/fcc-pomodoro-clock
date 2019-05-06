055290de5446958e41f17460656fca1c2194881c
Mark Ulrich <mark.ulrich.86@gmail.com>
2019-05-06 01:54:28 -0500

    Control arrows functioning

--- src/components/BreakTimeControl.js
+++ src/components/BreakTimeControl.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 export class BreakTimeControl extends Component {
   render() {
+    const { increment, decrement } = this.props.controlMethods;
+
     return (
       <div>
         <h3 id='break-label'>Break Length</h3>
         <div id='break-length'>{this.props.breakLength}</div>
-        <div id='break-increment'>
+        <div id='break-increment' onClick={increment}>
           <i className='fa fa-arrow-up' />
         </div>
-        <div id='break-decrement'>
+        <div id='break-decrement' onClick={decrement}>
           <i className='fa fa-arrow-down' />
         </div>
       </div>
--- src/components/SessionTimeControl.js
+++ src/components/SessionTimeControl.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 export class SessionTimeControl extends Component {
   render() {
+    const { increment, decrement } = this.props.controlMethods;
+
     return (
       <div>
         <h3 id='session-label'>Session Length</h3>
         <div id='session-length'>{this.props.sessionLength}</div>
-        <div id='session-increment'>
+        <div id='session-increment' onClick={increment}>
           <i className='fa fa-arrow-up' />
         </div>
-        <div id='session-decrement'>
+        <div id='session-decrement' onClick={decrement}>
           <i className='fa fa-arrow-down' />
         </div>
       </div>
